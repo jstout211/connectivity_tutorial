f7e5500aa0f4a08a4fb93f086f557cc12ecd0c79
Jeff Stout <stoutjd@nih.gov>
2022-08-12 09:16:35 -0400

    Runnable

--- plot_dataset_mne.py
+++ plot_dataset_mne.py
@@ -33,51 +33,59 @@ import mne
 
 subjects = glob.glob(root+'/sub-*')
 subjects = [os.path.basename(i).replace('sub-','') for i in subjects]
-subject = subjects[0]
-fs_subject = 'sub-'+subject
-
-bids_path = BIDSPath(subject=subject, session='01', task='sternberg', run='01',
-                     root=root, datatype='meg', suffix='meg')
-epochs_path = bids_path.copy().update(suffix='epo', extension='fif', processing='clean', run=None, check=False)
-epochs = mne.read_epochs(epochs_path.fpath)
-inv_bids_path = bids_path.copy().update(suffix='inv', extension='fif', run=None, check=False)
-inv = read_inverse_operator(inv_bids_path.fpath)
-fwd_bids_path = bids_path.copy().update(suffix='fwd', extension='fif', run=None,check=False)
-fwd = mne.read_forward_solution(fwd_bids_path.fpath)
 
 
-snr = 1.0
-lambda2 = 1.0 / snr ** 2
-stcs = apply_inverse_epochs(epochs, inv, lambda2, 'MNE', verbose=True)
-stcs = [abs(i) for i in stcs]
+def preproc_frites(subject):
+    fs_subject = 'sub-'+subject
+    bids_path = BIDSPath(subject=subject, session='01', task='sternberg', run='01',
+                         root=root, datatype='meg', suffix='meg')
+    epochs_path = bids_path.copy().update(suffix='epo', extension='fif', processing='clean', run=None, check=False)
+    epochs = mne.read_epochs(epochs_path.fpath)
+    inv_bids_path = bids_path.copy().update(suffix='inv', extension='fif', run=None, check=False)
+    inv = read_inverse_operator(inv_bids_path.fpath)
+    fwd_bids_path = bids_path.copy().update(suffix='fwd', extension='fif', run=None,check=False)
+    fwd = mne.read_forward_solution(fwd_bids_path.fpath)
+    
+    
+    snr = 1.0
+    lambda2 = 1.0 / snr ** 2
+    stcs = apply_inverse_epochs(epochs, inv, lambda2, 'MNE', verbose=True)
+    stcs = [abs(i) for i in stcs]
+    
+    labels_lh=mne.read_labels_from_annot(fs_subject, parc='aparc',
+                                        subjects_dir=subjects_dir, hemi='lh') 
+    labels_rh=mne.read_labels_from_annot(fs_subject, parc='aparc',
+                                        subjects_dir=subjects_dir, hemi='rh') 
+    labels=labels_lh + labels_rh 
+    label_names = [i.name for i in labels]
+    
+    label_ts = mne.extract_label_time_course(stcs, labels, fwd['src'], mode='pca_flip')
+    
+    #Convert list of numpy arrays to ndarray (Epoch/Label/Sample)
+    label_stack = np.stack(label_ts)
+    
+    sf = epochs.info['sfreq']
+    info = create_info(label_names, sf)
+    epoch = EpochsArray(label_stack, info, tmin=epochs.times[0], verbose=False)
+    epoch_frites_fname = os.path.join(frites_output_root, f'{subject}_epo.fif')
+    epoch.save(epoch_frites_fname)
 
-labels_lh=mne.read_labels_from_annot(fs_subject, parc='aparc',
-                                    subjects_dir=subjects_dir, hemi='lh') 
-labels_rh=mne.read_labels_from_annot(fs_subject, parc='aparc',
-                                    subjects_dir=subjects_dir, hemi='rh') 
-labels=labels_lh + labels_rh 
-label_names = [i.name for i in labels]
-
-label_ts = mne.extract_label_time_course(stcs, labels, fwd['src'], mode='pca_flip')
-
-#Convert list of numpy arrays to ndarray (Epoch/Label/Sample)
-label_stack = np.stack(label_ts)
-
-sf = epochs.info['sfreq']
-info = create_info(label_names, sf)
-epoch = EpochsArray(label_stack, info, tmin=epochs.times[0], verbose=False)
+failed_subjs=[]
+for subject in subjects:
+    try:
+        preproc_frites(subject)
+    except:
+        failed_subjs.append(subject)
 
 
-
-
-
-
+# =============================================================================
+# 
+# =============================================================================
 x_mne = []
-for k in range(n_subjects):
-    # create some informations
-    info = create_info(ch[k].tolist(), sf)
-    # create the Epoch of this subject
-    epoch = EpochsArray(x[k], info, tmin=times[0], verbose=False)
+epoch_files = glob.glob(frites_output_root+ '/*_epo.fif')
+sorted(epoch_files)
+for k in epoch_files:
+    epoch = mne.read_epochs(k)
     # finally, replace it in the original list
     x_mne.append(epoch)
 print(x_mne[0])
