2ea591c2f04b936ae4078a8ad63815faeaaa0b45
Jeff Stout <stoutjd@nih.gov>
2022-08-12 10:39:45 -0400

    ENH - load all epoch formatted stcs and create EPhysDataset

--- plot_dataset_mne.py
+++ plot_dataset_mne.py
@@ -15,6 +15,12 @@ from mne.minimum_norm import read_inverse_operator
 import os
 
 import matplotlib.pyplot as plt
+from mne_bids import BIDSPath
+import glob
+import os 
+import mne
+import pandas as pd
+
 set_mpl_style()
 
 # =============================================================================
@@ -26,13 +32,12 @@ subjects_dir =  '/data/jstout/HVderivatives/freesurfer/subjects'
 frites_output_root = os.path.join(os.path.dirname(root), 'frites_dset')
 if not os.path.exists(frites_output_root): os.mkdir(frites_output_root)
 
-from mne_bids import BIDSPath
-import glob
-import os 
-import mne
-
 subjects = glob.glob(root+'/sub-*')
 subjects = [os.path.basename(i).replace('sub-','') for i in subjects]
+
+
+# def write_epoch_file(
+
 
 
 def preproc_frites(subject):
@@ -46,11 +51,8 @@ def preproc_frites(subject):
     fwd_bids_path = bids_path.copy().update(suffix='fwd', extension='fif', run=None,check=False)
     fwd = mne.read_forward_solution(fwd_bids_path.fpath)
     
-    
     snr = 1.0
     lambda2 = 1.0 / snr ** 2
-    stcs = apply_inverse_epochs(epochs, inv, lambda2, 'MNE', verbose=True)
-    stcs = [abs(i) for i in stcs]
     
     labels_lh=mne.read_labels_from_annot(fs_subject, parc='aparc',
                                         subjects_dir=subjects_dir, hemi='lh') 
@@ -58,6 +60,11 @@ def preproc_frites(subject):
                                         subjects_dir=subjects_dir, hemi='rh') 
     labels=labels_lh + labels_rh 
     label_names = [i.name for i in labels]
+
+    ## Encode4 processing
+    stcs = apply_inverse_epochs(epochs['encode4'], inv, lambda2, 'MNE', verbose=True)
+    [i.apply_baseline((-0.4, 0)) for i in stcs]  #HACK - not sure why this is needed
+#    stcs = [abs(i) for i in stcs]
     
     label_ts = mne.extract_label_time_course(stcs, labels, fwd['src'], mode='pca_flip')
     
@@ -66,7 +73,31 @@ def preproc_frites(subject):
     
     sf = epochs.info['sfreq']
     info = create_info(label_names, sf)
-    epoch = EpochsArray(label_stack, info, tmin=epochs.times[0], verbose=False)
+    epoch_encode4 = EpochsArray(label_stack, info, tmin=epochs.times[0],
+                                verbose=False)
+    epoch_encode4.events[:,2]*=4
+    epoch_encode4.event_id={'4':4}
+    
+    ## Encode6 processing
+    stcs = apply_inverse_epochs(epochs['encode6'], inv, lambda2, 'MNE', verbose=True)
+    [i.apply_baseline((-0.4, 0)) for i in stcs]  #HACK - not sure why this is needed
+#    stcs = [abs(i) for i in stcs]
+    
+    label_ts = mne.extract_label_time_course(stcs, labels, fwd['src'], mode='pca_flip')
+    
+    #Convert list of numpy arrays to ndarray (Epoch/Label/Sample)
+    label_stack = np.stack(label_ts)
+    
+    sf = epochs.info['sfreq']
+    info = create_info(label_names, sf)
+    epoch_encode6 = EpochsArray(label_stack, info, tmin=epochs.times[0], 
+                                verbose=False)   
+    epoch_encode6.events[:,2]*=6
+    epoch_encode6.event_id={'6':6}
+    
+    ## Combine and save
+    epoch = mne.concatenate_epochs([epoch_encode4,epoch_encode6])
+    
     epoch_frites_fname = os.path.join(frites_output_root, f'{subject}_epo.fif')
     epoch.save(epoch_frites_fname)
 
@@ -97,8 +128,22 @@ print(x_mne[0])
 # Finally, we pass the data to the :class:`frites.dataset.DatasetEphy` class
 # in order to create the dataset
 
-dt = DatasetEphy(x_mne)
-print(dt)
+labels_fname = os.path.join(frites_output_root, 'ROIs_DK.csv')
+#Save out the roi names if not done
+#pd.DataFrame(label_names, columns=['ROInames']).to_csv(labels_fname,index=False)
+rois = pd.read_csv(labels_fname).ROInames.tolist()
 
+times_fname = os.path.join(frites_output_root, 'Epoch_times.csv')
+#pd.DataFrame(epoch.times, columns=['Times']).to_csv(times_fname,index=False)
+times = pd.read_csv(times_fname).Times.tolist()
+
+dt = DatasetEphy(x_mne, 
+                 y = [tmp_.events[:,2] for tmp_ in x_mne],
+                 roi=[rois for i in range(len(x_mne))],
+                 times=times)
+
+
+#dt = DatasetEphy(x_mne)
+print(dt)
 print('Time vector : ', dt.times)
 print('ROI DataFrame\n: ', dt.df_rs)
